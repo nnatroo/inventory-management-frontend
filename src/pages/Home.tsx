e3309d6e3dd8f0e1a8ef9cddfd480e5b116c951c
nnatroo <nugonatroshvili@gmail.com>
2023-05-07 20:15:59 +0400

    added sortType asc and desc

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,18 +1,20 @@
-import React, { useEffect, useState } from 'react'
-import Table from '../components/Table'
-import Pagination from '../components/Pagination'
-import Item from '../models/item'
+import React, { useEffect, useState } from "react";
+import Table from "../components/Table";
+import Pagination from "../components/Pagination";
+import Item from "../models/item";
 
 const Home: React.FC = () => {
-
   const [tableData, setTableData] = useState<Item[]>([]);
   const [currentPage, setCurrentPage] = useState(1);
   const [totalItems, setTotalItems] = useState(0);
   const [dataChanged, setDataChanged] = useState(1);
-  const [sortOption, setSortOption] = useState("name")
+  const [sortOption, setSortOption] = useState("name");
+  const [sortType, setSortType] = useState("asc");
 
   useEffect(() => {
-    fetch(`http://localhost:3000/inventories?sort=${sortOption}`)
+    fetch(
+      `http://localhost:3000/inventories?sort=${sortOption}&type=${sortType}`
+    )
       .then((response) => response.json())
       .then((data) => {
         setTableData(data);
@@ -20,14 +22,6 @@ const Home: React.FC = () => {
       })
       .catch((error) => console.error(error));
   }, [dataChanged]);
-
-  const nextPageHandler = () => {
-    setCurrentPage(currentPage + 1);
-  }
-
-  const prevPageHandler = () => {
-    setCurrentPage(currentPage - 1);
-  }
 
   const deleteHandler = (itemId: number) => {
     const inventoryID = itemId;
@@ -40,30 +34,66 @@ const Home: React.FC = () => {
           throw new Error("Request failed");
         }
         console.log("Inventory deleted successfully");
-        setDataChanged(dataChanged + 1)
-
+        setDataChanged(dataChanged + 1);
       })
       .catch((error) => {
         console.error("Error deleting inventory:", error);
       });
-  }
+  };
+
+  const nextPageHandler = () => {
+    setCurrentPage(currentPage + 1);
+  };
+
+  const prevPageHandler = () => {
+    setCurrentPage(currentPage - 1);
+  };
 
   const priceSortHandler = () => {
-    setSortOption("price");
-    setDataChanged(dataChanged + 1);
-  }
+    if (sortOption == "price") {
+      if (sortType == "asc") {
+        setSortType("desc");
+      } else {
+        setSortType("asc");
+      }
+      setDataChanged(dataChanged + 1);
+    } else {
+      setSortOption("price");
+      setDataChanged(dataChanged + 1);
+    }
+  };
 
   const nameSortHandler = () => {
-    setSortOption("name");
-    setDataChanged(dataChanged + 1);
-  }
+    if (sortOption == "name") {
+      if (sortType == "asc") {
+        setSortType("desc");
+      } else {
+        setSortType("asc");
+      }
+      setDataChanged(dataChanged + 1);
+    } else {
+      setSortOption("name");
+      setDataChanged(dataChanged + 1);
+    }
+  };
 
   return (
     <>
-      <Table onPriceSort={priceSortHandler} onNameSort={nameSortHandler} onDelete={deleteHandler} tableData={tableData} currentPage={currentPage} />
-      <Pagination totalItems={totalItems} currentPage={currentPage} onNextPage={nextPageHandler} onPrevPage={prevPageHandler} />
+      <Table
+        onPriceSort={priceSortHandler}
+        onNameSort={nameSortHandler}
+        onDelete={deleteHandler}
+        tableData={tableData}
+        currentPage={currentPage}
+      />
+      <Pagination
+        totalItems={totalItems}
+        currentPage={currentPage}
+        onNextPage={nextPageHandler}
+        onPrevPage={prevPageHandler}
+      />
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
